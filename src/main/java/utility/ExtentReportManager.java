d2d402d135aeaaeceaf09d952859be92fc93142b
Veranika Alifer <veranika.alifer@gmail.com>
2025-02-28 15:59:03 +0100

    add ExtentReportManager class for report generation

---
+++ src/main/java/utility/ExtentReportManager.java
@@ -0,0 +1,19 @@
+package utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager {
+    private static final ExtentReports extentReports = new ExtentReports();
+
+    static {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("reports/ExtentReport.html");
+        reporter.config().setDocumentTitle("Extent report");
+        reporter.config().setReportName("Simple tests");
+        extentReports.attachReporter(reporter);
+    }
+
+    public static ExtentReports getInstance(){
+        return extentReports;
+    }
+}
